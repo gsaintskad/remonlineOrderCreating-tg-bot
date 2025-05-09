d258a3b0fbe9a638da7dab7fbb6aab7ef22aac67
Hlib Bezuhlyi <144724842+gsaintskad@users.noreply.github.com>
2025-05-09 17:39:54 +0200

    pre deploy

--- tgbot/telegram/scenes/scene.get-orders.mjs
+++ tgbot/telegram/scenes/scene.get-orders.mjs
@@ -1,32 +1,37 @@
-import {Markup, Scenes} from "telegraf";
-import {ua} from "../../translate.mjs";
-import {getOrders} from "../../remonline/remonline.utils.mjs";
+import { Markup, Scenes } from 'telegraf';
+import { ua } from '../../translate.mjs';
+import { getOrders } from '../../remonline/remonline.utils.mjs';
 
-const webAppUrl="https://da0e-83-25-239-178.ngrok-free.app/";
+const webAppUrl = process.env.TELEGRAM_WEB_APP;
 export const getOrdersScene = new Scenes.WizardScene(
-    process.env.GET_ORDERS_SCENE,
-    async (ctx) => {
-        console.log("open web_app...",ctx.session.remonline_id);
-        ctx.reply(
-            ua.getOrders.initText,
+  process.env.GET_ORDERS_SCENE,
+  async (ctx) => {
+    console.log('open web_app...', ctx.session.remonline_id);
+    ctx.reply(
+      ua.getOrders.initText,
 
-            Markup.inlineKeyboard([
-                [Markup.button.webApp(ua.getOrders.openOrderListBtn, `${webAppUrl}/?remonline_id=${ctx.session.remonline_id}`)],
-            ])
-        );
-        return ctx.wizard.next();
-    },
-    async (ctx) => {
-        const response=await getOrders();
-        console.log(response);
-        return ctx.wizard.next();
-    },
-    async (ctx) => {
-        const { data, from } = ctx.update?.callback_query || {}
-        if(data==='open_web_app'){
-            // console.log("open web_app...",            ctx.wizard.userData.remonlin_id);
-            // ctx.wizard.userData.remonlin_id;
-            return ctx.wizard.next();
-        }
+      Markup.inlineKeyboard([
+        [
+          Markup.button.webApp(
+            ua.getOrders.openOrderListBtn,
+            `${webAppUrl}/?remonline_id=${ctx.session.remonline_id}`
+          ),
+        ],
+      ])
+    );
+    return ctx.wizard.next();
+  },
+  async (ctx) => {
+    const response = await getOrders();
+    console.log(response);
+    return ctx.wizard.next();
+  },
+  async (ctx) => {
+    const { data, from } = ctx.update?.callback_query || {};
+    if (data === 'open_web_app') {
+      // console.log("open web_app...",            ctx.wizard.userData.remonlin_id);
+      // ctx.wizard.userData.remonlin_id;
+      return ctx.wizard.next();
     }
-)
+  }
+);
