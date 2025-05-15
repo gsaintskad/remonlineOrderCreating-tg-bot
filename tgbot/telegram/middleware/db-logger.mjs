1bd523d7dd5616d345598f19d16dcf7aeb795cdc
Hlib Bezuhlyi <144724842+gsaintskad@users.noreply.github.com>
2025-05-15 16:36:38 +0200

    ctx log

--- tgbot/telegram/middleware/db-logger.mjs
+++ tgbot/telegram/middleware/db-logger.mjs
@@ -1,33 +1,30 @@
-import {
-    isUserSaved,
-    saveNewUser
-} from '../telegram.queries.mjs';
+import { isUserSaved, saveNewUser } from '../telegram.queries.mjs';
 
 export async function dbLogger(ctx, next) {
-    return
-    if (ctx.session.saved) {
-        await next();
-        return
-    }
-    const { message, callback_query } = ctx.update;
-    const { from, text } = message || callback_query || inline_query;
-    const { id, first_name, last_name, username } = from
-    const result = await isUserSaved({ id })
-    if (result) {
-        ctx.session.saved = true
-        ctx.session.remonline_id = result.remonline_id
-        ctx.session.telegram_name = result.first_name
-
-        ctx.session.branch_id = result.branch_id
-        ctx.session.branch_public_name = result.branch_public_name
-
-        await next();
-        return
-    }
-    await saveNewUser({ id, first_name, last_name, username })
-    console.log(`New user saved ${id}`)
-    ctx.session.saved = true
-    ctx.session.telegram_name = first_name
+  console.log('\n\n\n\n\n\n', ctx, '\n\n\n\n\n\n');
+  if (ctx.session.saved) {
     await next();
-    return
+    return;
+  }
+  const { message, callback_query } = ctx.update;
+  const { from, text } = message || callback_query || inline_query;
+  const { id, first_name, last_name, username } = from;
+  const result = await isUserSaved({ id });
+  if (result) {
+    ctx.session.saved = true;
+    ctx.session.remonline_id = result.remonline_id;
+    ctx.session.telegram_name = result.first_name;
+
+    ctx.session.branch_id = result.branch_id;
+    ctx.session.branch_public_name = result.branch_public_name;
+
+    await next();
+    return;
+  }
+  await saveNewUser({ id, first_name, last_name, username });
+  console.log(`New user saved ${id}`);
+  ctx.session.saved = true;
+  ctx.session.telegram_name = first_name;
+  await next();
+  return;
 }
